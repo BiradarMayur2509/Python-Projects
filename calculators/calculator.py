f95773adff4c353947befd7c66196253904d2d6d
BiradarMayur2509 <biradarmayur2509@gmail.com>
2025-12-11 10:12:25 +0530

    calculator.py - step 3 (choose operation)

--- calculators/calculator.py
+++ calculators/calculator.py
@@ -6,8 +6,27 @@ def get_number(prompt):
         except ValueError:
             print("Invalid input. Please enter a valid number.")
             
+print("Select operation:")
+print("1. Add (+)")
+print("2. Subtract (-)")
+print("3. Multiply (*)")
+print("4. Divide (/)")
+
+choice = input("Enter choice (1/2/3/4): ").strip()
+            
 a = get_number("Enter the first number: ")
 b = get_number("Enter the second number: ")
 
-print("Numbers are:", a, "and", b)
-print(Add:, a + b)
+if choice == '1':
+    print("result:", a + b)
+elif choice == '2':
+    print("result:", a - b)
+elif choice == '3':
+    print("result:", a * b)
+elif choice == '4':
+    if b == 0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        print("result:", a / b)
+else:
+    print("Invalid choice.")
