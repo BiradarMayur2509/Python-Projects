d078aae33a9a980ce6e806a22f201259d4d38fa3
BiradarMayur2509 <biradarmayur2509@gmail.com>
2026-04-13 20:50:03 +0530

    #

---
+++ files-sorter/main.py
@@ -0,0 +1,12 @@
+import os 
+import shutil
+
+# step1 : select folder
+
+folder_path = ""  # change this according to u
+
+# step 2: file type categories
+extensions = {
+    "Images" : [".jpg", ".png", ".jpeg"],
+    "videos" : [""]
+}
