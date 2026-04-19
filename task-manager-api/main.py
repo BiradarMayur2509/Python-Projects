3c838f9a68b557191b9792b5f70c02692e6b62ff
BiradarMayur2509 <biradarmayur2509@gmail.com>
2026-04-19 10:40:03 +0530

    Adding task features

--- task-manager-api/main.py
+++ task-manager-api/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 
-APP = FastAPI()
+app = FastAPI()
 
 @app.get("/")
-
 def home():
     return {"message": "Task Manager API is running"}
+
+
+@app.get('/tasks')
+def get_tasks():
+    return {"Message": "task Manager API is running"}
+
+@app.post('/tasks')
+def add_task(task: str):
+    tasks.append(task)
+    return {"message": "task added", "task": task}
+
